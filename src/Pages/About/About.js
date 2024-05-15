ad020040af762d66b12cfa5b4fabe4f8d98bb387
meerimjm <meerimzumanova5@gmail.com>
2024-05-15 19:28:53 +0300

    wrote the html code in the section about

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,64 @@ import './About.css';
 function About() {
   return (
     <>
-      About
+      <section className='about'>
+        <div className="container">
+          <h2 className="about_tit">О нас</h2>
+          <p className="about_txt">Желаете купить цветы в Бишкеке? Или заказать цветочную композицию с доставкой через Интернет? Добро пожаловать в мир цветов Camelia.kg
+
+Camelia.kg — цветочный интернет-магазин в Бишкеке. Мы предоставляем доставку цветов и подарков по Кыргызстану, странам СНГ и всему миру!
+
+Команда Camelia.kg во флористическом бизнесе с 2015 года. За это время мы смогли войти в топовую 5-ку самых лучших цветочных магазинов Бишкека, благодаря качеству цветов и первоклассному сервису, которые предлагает компания Camelia.
+
+ 
+
+Став нашим клиентом, Вы гарантированно получаете:</p>
+          <ul>
+            <li> 
+              <div className="about_ic flex">
+                <img src="" alt="" />
+                <p className="about_txt">исполнение заказа и доставку в обозначенный срок;</p>
+              </div>
+            </li>
+            <li> 
+              <div className="about_ic flex">
+                <img src="" alt="" />
+                <p className="about_txt">исполнение заказа и доставку в обозначенный срок;</p>
+              </div>
+            </li>
+            <li> 
+              <div className="about_ic flex">
+                <img src="" alt="" />
+                <p className="about_txt">исполнение заказа и доставку в обозначенный срок;</p>
+              </div>
+            </li>
+            <li> 
+              <div className="about_ic flex">
+                <img src="" alt="" />
+                <p className="about_txt">исполнение заказа и доставку в обозначенный срок;</p>
+              </div>
+            </li>
+            <li> 
+              <div className="about_ic flex">
+                <img src="" alt="" />
+                <p className="about_txt">исполнение заказа и доставку в обозначенный срок;</p>
+              </div>
+            </li>
+            <li> 
+              <div className="about_ic flex">
+                <img src="" alt="" />
+                <p className="about_txt">исполнение заказа и доставку в обозначенный срок;</p>
+              </div>
+            </li>
+            <li> 
+              <div className="about_ic flex">
+                <img src="" alt="" />
+                <p className="about_txt">исполнение заказа и доставку в обозначенный срок;</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
     </>
   );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,10 +3,26 @@ import './Home.css';
 function Home() {
   return (
     <>
-      <section className="">
+      <section className="flofers flex">
         <div className='container'>
-          <h2>Доставка цветов в Караколе</h2>
-          <p>Режим работы 24/7</p>
+          <h2 className='sec_tit'>Доставка цветов в Караколе</h2>
+          <p className='flofers_txt'>Режим работы 24/7</p>
+        </div>
+      </section>
+      <section>
+        <div className='container'>
+          <div className='bouquet_cards flex'>
+            <div className='bouquet_card'>
+              <div className='bouquet_img'>
+                <img src="" alt="" />
+              </div>
+              <p className="card_txt">Букет</p>
+              <div className='card_tit'>"Нежность"</div>
+              <div className='card_price'>2500с</div>
+
+            </div>
+          </div>
+
         </div>
       </section>
     </>
