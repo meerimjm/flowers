2e009aa09bd8cead40beee71b738c87faa67584f
meerimjm <meerimzumanova5@gmail.com>
2024-05-16 12:29:45 +0300

    a or to link

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 function Footer() {
@@ -7,57 +8,57 @@ function Footer() {
 		<div class="container">
 			<div class="footer_in flex">
 				<div class="footer_info">
-					<a href="index.html" class="footer_logo">
+					<Link to="index.html" class="footer_logo">
 						<img src="img/logo-footer.svg" alt=""/>
-					</a>
+					</Link>
 					<p class="footer_txt">In the new era of technology we look a in the future with certainty and pride
 						to for our company and.</p>
 					<div class="footer_icons flex">
-						<a href="" class="footer_ic footer_ic__tw">
+						<Link to="" class="footer_ic footer_ic__tw">
 							<img src="img/icons/footer_ic__tw.svg" alt=""/>
-						</a>
-						<a href="" class="footer_ic footer_ic__fb">
+						</Link>
+						<Link to="" class="footer_ic footer_ic__fb">
 							<img src="img/icons/footer_ic__fb.svg" alt=""/>
-						</a>
-						<a href="" class="footer_ic footer_ic__inst">
+						</Link>
+						<Link to="" class="footer_ic footer_ic__inst">
 							<img src="img/icons/footer_ic__inst.svg" alt=""/>
-						</a>
-						<a href="" class="footer_ic footer_ic__git">
+						</Link>
+						<Link to="" class="footer_ic footer_ic__git">
 							<img src="img/icons/footer_ic__tw.svg" alt=""/>
-						</a>
+						</Link>
 					</div>
 				</div>
 				<div class="footer_menu">
 					<h4 class="footer_tit">Pages</h4>
 					<ul class="footer_list">
-						<li><a href="">Home</a></li>
-						<li><a href="">About</a></li>
-						<li><a href="">Menu</a></li>
-						<li><a href="">Pricing</a></li>
-						<li><a href="">Blog</a></li>
-						<li><a href="">Contact</a></li>
-						<li><a href="">Delivery</a></li>
+						<li><Link to="">Home</Link></li>
+						<li><Link to="">About</Link></li>
+						<li><Link to="">Menu</Link></li>
+						<li><Link to="">Pricing</Link></li>
+						<li><Link to="">Blog</Link></li>
+						<li><Link to="">Contact</Link></li>
+						<li><Link to="">Delivery</Link></li>
 					</ul>
 				</div>
 				<div class="footer_menu">
 					<h4 class="footer_tit">Utility Pages</h4>
 					<ul class="footer_list">
-						<li><a href="">Start Here</a></li>
-						<li><a href="">Styleguide</a></li>
-						<li><a href="">Password Protected</a></li>
-						<li><a href="">404 Not Found</a></li>
-						<li><a href="">Licenses</a></li>
-						<li><a href="">Changelog</a></li>
-						<li><a href="">View More</a></li>
+						<li><Link to="">Start Here</Link></li>
+						<li><Link to="">Styleguide</Link></li>
+						<li><Link to="">Password Protected</Link></li>
+						<li><Link to="">404 Not Found</Link></li>
+						<li><Link to="">Licenses</Link></li>
+						<li><Link to="">Changelog</Link></li>
+						<li><Link to="">View More</Link></li>
 					</ul>
 				</div>
 				<div class="footer_imgs">
 					<h4 class="footer_tit">Follow Us On Instagram</h4>
 					<div class="footer_img-wrap flex ">
-						<a href="" class="footer_img"><img src="img/foto-1.jpg" alt=""/></a>
-						<a href="" class="footer_img"><img src="img/foto-2.jpg" alt=""/></a>
-						<a href="" class="footer_img"><img src="img/foto-3.jpg" alt=""/></a>
-						<a href="" class="footer_img"><img src="img/foto-4.jpg" alt=""/></a>
+						<Link to="" class="footer_img"><img src="img/foto-1.jpg" alt=""/></Link>
+						<Link to="" class="footer_img"><img src="img/foto-2.jpg" alt=""/></Link>
+						<Link to="" class="footer_img"><img src="img/foto-3.jpg" alt=""/></Link>
+						<Link to="" class="footer_img"><img src="img/foto-4.jpg" alt=""/></Link>
 					</div>
 				</div>
 			</div>
