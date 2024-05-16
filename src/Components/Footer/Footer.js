be97dab909481bc8f54d1d1b722113ef0e2f58e1
meerimjm <meerimzumanova5@gmail.com>
2024-05-16 12:34:29 +0300

    changed the class style on class

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,32 +5,32 @@ function Footer() {
   return (
     <>
       <footer>
-		<div class="container">
-			<div class="footer_in flex">
-				<div class="footer_info">
-					<Link to="index.html" class="footer_logo">
+		<div className="container">
+			<div className="footer_in flex">
+				<div classNameName="footer_info">
+					<Link to="index.html" className="footer_logo">
 						<img src="img/logo-footer.svg" alt=""/>
 					</Link>
-					<p class="footer_txt">In the new era of technology we look a in the future with certainty and pride
+					<p className="footer_txt">In the new era of technology we look a in the future with certainty and pride
 						to for our company and.</p>
-					<div class="footer_icons flex">
-						<Link to="" class="footer_ic footer_ic__tw">
+					<div className="footer_icons flex">
+						<Link to="" className="footer_ic footer_ic__tw">
 							<img src="img/icons/footer_ic__tw.svg" alt=""/>
 						</Link>
-						<Link to="" class="footer_ic footer_ic__fb">
+						<Link to="" className="footer_ic footer_ic__fb">
 							<img src="img/icons/footer_ic__fb.svg" alt=""/>
 						</Link>
-						<Link to="" class="footer_ic footer_ic__inst">
+						<Link to="" className="footer_ic footer_ic__inst">
 							<img src="img/icons/footer_ic__inst.svg" alt=""/>
 						</Link>
-						<Link to="" class="footer_ic footer_ic__git">
+						<Link to="" className="footer_ic footer_ic__git">
 							<img src="img/icons/footer_ic__tw.svg" alt=""/>
 						</Link>
 					</div>
 				</div>
-				<div class="footer_menu">
-					<h4 class="footer_tit">Pages</h4>
-					<ul class="footer_list">
+				<div className="footer_menu">
+					<h4 className="footer_tit">Pages</h4>
+					<ul className="footer_list">
 						<li><Link to="">Home</Link></li>
 						<li><Link to="">About</Link></li>
 						<li><Link to="">Menu</Link></li>
@@ -40,9 +40,9 @@ function Footer() {
 						<li><Link to="">Delivery</Link></li>
 					</ul>
 				</div>
-				<div class="footer_menu">
-					<h4 class="footer_tit">Utility Pages</h4>
-					<ul class="footer_list">
+				<div className="footer_menu">
+					<h4 className="footer_tit">Utility Pages</h4>
+					<ul className="footer_list">
 						<li><Link to="">Start Here</Link></li>
 						<li><Link to="">Styleguide</Link></li>
 						<li><Link to="">Password Protected</Link></li>
@@ -52,17 +52,17 @@ function Footer() {
 						<li><Link to="">View More</Link></li>
 					</ul>
 				</div>
-				<div class="footer_imgs">
-					<h4 class="footer_tit">Follow Us On Instagram</h4>
-					<div class="footer_img-wrap flex ">
-						<Link to="" class="footer_img"><img src="img/foto-1.jpg" alt=""/></Link>
-						<Link to="" class="footer_img"><img src="img/foto-2.jpg" alt=""/></Link>
-						<Link to="" class="footer_img"><img src="img/foto-3.jpg" alt=""/></Link>
-						<Link to="" class="footer_img"><img src="img/foto-4.jpg" alt=""/></Link>
+				<div className="footer_imgs">
+					<h4 className="footer_tit">Follow Us On Instagram</h4>
+					<div className="footer_img-wrap flex ">
+						<Link to="" className="footer_img"><img src="img/foto-1.jpg" alt=""/></Link>
+						<Link to="" className="footer_img"><img src="img/foto-2.jpg" alt=""/></Link>
+						<Link to="" className="footer_img"><img src="img/foto-3.jpg" alt=""/></Link>
+						<Link to="" className="footer_img"><img src="img/foto-4.jpg" alt=""/></Link>
 					</div>
 				</div>
 			</div>
-			<p class="footer_copy">Copyright © 2023 Hashtag Developer. All Rights Reserved</p>
+			<p className="footer_copy">Copyright © 2023 Hashtag Developer. All Rights Reserved</p>
 
 		</div>
 	</footer>
